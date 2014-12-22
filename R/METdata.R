33e3979f814b68069400ebbdbd929cc92b3de0ef
Raul Eyzaguirre <reyzaguirre@gmail.com>
2014-12-22 14:10:19 -0500

    data added

---
+++ R/METdata.R
@@ -0,0 +1,10 @@
+#' @name METdata
+#' @title Yields for genotypes in different environments
+#' @docType data
+#' @aliases METdata
+#' @description This data set gives the yields per plot for 8 genotypes
+#' in 12 environments with a RCBD with 3 blocks in each environment.
+#' @usage METdata
+#' @format A data frame with 4 columns and 288 rows.
+#' @source International Potato Center, sweetpotato experimental data.
+NULL
