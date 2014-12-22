ffa3325cecb1bdb95e854a030577dfc3b8ab7370
Raul Eyzaguirre <reyzaguirre@gmail.com>
2014-12-22 11:15:38 -0500

    labels

--- R/AMMI.R
+++ R/AMMI.R
@@ -13,7 +13,7 @@
 #' @param biplot1 Choose "effects" or "means" for biplot1.
 #' @param title Main title for biplot1 or biplot2.
 #' @param xlab Xlab for biplot1.
-#' @param color Color for lines, symbols and names for environments, genotypes and axes.
+#' @param color Color for lines, symbols and/or labels for environments, genotypes and axes.
 #' @param ... Additional graphic parameters.
 #' @author Raul Eyzaguirre
 #' @details Significance of PCs are evaluated only with \code{method = "AMMI"} and if
@@ -26,7 +26,7 @@
 #' @references
 #' Gollob, H. R. (1968). A Statistical Model which combines Features of Factor Analytic
 #' and Analysis of Variance Techniques, Psychometrika, Vol 33(1): 73-114.
-#' 
+#'
 #' Yan, W. et al. (2000). Cultivar evaluation and mega-environment investigation based on the GGE
 #' biplot, Crop Sci., Vol 40: 597-605.
 #' @seealso \code{svd}
@@ -34,10 +34,10 @@
 #' # Load data
 #' urlfile <- "https://raw.githubusercontent.com/SweetPotatoImprov/StatTools/master/AMMI/METdata.dat"
 #' mydata <- read.table(urlfile, header=T)
-#' 
+#'
 #' # Run AMMI for trait y, biplot1 by default
 #' AMMI("y", "geno", "env", "rep", mydata)
-#' 
+#'
 #' # Run AMMI for trait y, biplot2
 #' AMMI("y", "geno", "env", "rep", mydata, biplot = 2)
 #' @export
@@ -45,44 +45,44 @@
 AMMI <- function(trait, geno, env, rep, data, method = "AMMI", f = .5,
                  biplot = 1, biplot1 = "effects", title = NULL, xlab = NULL,
                  color = c("darkorange", "black", "gray"), ...){
-  
+
   # Everything as factor
-  
+
   data[,geno] <- factor(data[,geno])
   data[,env] <- factor(data[,env])
   data[,rep] <- factor(data[,rep])
 
   # Check data
-  
+
   lc <- CheckData02(trait, geno, env, data)
-  
+
   # Error messages
-  
+
   if (lc$c1 == 0)
     stop("Error: Some GxE cells have zero frequency. Remove genotypes or environments to proceed.")
-  
+
   if (lc$c1 == 1 & lc$c2 == 0)
     warning("Warning: There is only one replication. Inference is not possible with one replication.")
-  
+
   if (method == "AMMI" & lc$c1 == 1 & lc$c2 == 1 & lc$c3 == 0)
     warning("Warning: The data set is unbalanced. Significance of PCs is not evaluated.")
 
   geno.num <- nlevels(data[,geno])
   env.num <- nlevels(data[,env])
   rep.num <- nlevels(data[,rep])
-  
+
   if (geno.num < 2 | env.num < 2)
     stop(paste("Error: This is not a MET experiment."))
-  
+
   if (geno.num < 3 | env.num < 3)
     stop(paste("Error: You need at least 3 genotypes and 3 environments to run AMMI or GGE."))
-  
+
   # Compute interaction means matrix
-  
+
   int.mean <- tapply(data[,trait], list(data[,geno], data[,env]), mean, na.rm = T)
-  
+
   # Compute ANOVA
-  
+
   if (lc$c1 + lc$c2 + lc$c3 == 3){
     model <- aov(data[,trait] ~ data[,geno] + data[,env] +
                    data[,rep] %in% data[,env] + data[,geno]:data[,env])
@@ -93,9 +93,9 @@ AMMI <- function(trait, geno, env, rep, data, method = "AMMI", f = .5,
     rdf <- NULL
     rms <- NULL
   }
-  
+
   # Run AMMIwithMeans
-  
+
   AMMIwithMeans(int.mean, trait = trait, rep.num = rep.num, rdf = rdf, rms = rms,
                 method = method, f = f, biplot = biplot, biplot1 = biplot1,
                 title = title, xlab = xlab, color = color, ...)
@@ -116,11 +116,11 @@ AMMI <- function(trait, geno, env, rep, data, method = "AMMI", f = .5,
 #' @param biplot1 Choose "effects" or "means" for biplot1.
 #' @param title Main title for biplot1 or biplot2.
 #' @param xlab Xlab for biplot1.
-#' @param color Color for lines, symbols and names for environments, genotypes and axes.
+#' @param color Color for lines, symbols and/or labels for environments, genotypes and axes.
 #' @param ... Additional graphic parameters.
 #' @author Raul Eyzaguirre
 #' @details Significance of PCs are evaluated only with \code{method = "AMMI"} and if
-#' \code{rep.num}, \code{rms} and \code{rdf} are specified. 
+#' \code{rep.num}, \code{rms} and \code{rdf} are specified.
 #' @return It returns the first and second PC values for genotypes and environments,
 #' a table with the contribution of each PC, a dispersion plot of means or effects
 #' against the first PC, or a dispersion plot of PC1 against PC2.
@@ -129,7 +129,7 @@ AMMI <- function(trait, geno, env, rep, data, method = "AMMI", f = .5,
 #' @references
 #' Gollob, H. R. (1968). A Statistical Model which combines Features of Factor Analytic
 #' and Analysis of Variance Techniques, Psychometrika, Vol 33(1): 73-114.
-#' 
+#'
 #' Yan, W. et al. (2000). Cultivar evaluation and mega-environment investigation based on the GGE
 #' biplot, Crop Sci., Vol 40: 597-605.
 #' @seealso \code{svd}
@@ -137,13 +137,13 @@ AMMI <- function(trait, geno, env, rep, data, method = "AMMI", f = .5,
 #' # Load data
 #' urlfile <- "https://raw.githubusercontent.com/SweetPotatoImprov/StatTools/master/AMMI/METdata.dat"
 #' mydata <- read.table(urlfile, header=T)
-#' 
+#'
 #' # Compute GxE means
 #' int.mean <- tapply(mydata$y, list(mydata$geno, mydata$env), mean, na.rm = T)
-#' 
+#'
 #' # Run AMMI with GxE means matrix, biplot2
 #' AMMIwithMeans(int.mean, trait = "y", biplot = 2)
-#' 
+#'
 #' # Run GGE with GxE means matrix, biplot2
 #' AMMIwithMeans(int.mean, trait = "y", method = "GGE", biplot = 2)
 #' @export
@@ -152,28 +152,28 @@ AMMIwithMeans <- function(int.mean, trait = NULL, rep.num = NULL, rdf = NULL,
                           rms = NULL, method = "AMMI", f = .5, biplot = 1,
                           biplot1 = "effects", title = NULL, xlab = NULL,
                           color = c("darkorange", "black", "gray"), ...){
-  
+
   # Data
-  
+
   overall.mean <- mean(int.mean)
   env.mean <- apply(int.mean, 2, mean)
   geno.mean <- apply(int.mean, 1, mean)
   env.num <- length(env.mean)
   geno.num <- length(geno.mean)
-  
+
   if (method == "AMMI"){
     svd.mat <- int.mean + overall.mean
     for (i in 1:env.num) svd.mat[,i] <- svd.mat[,i] - geno.mean
-    for (i in 1:geno.num) svd.mat[i,] <- svd.mat[i,] - env.mean      
+    for (i in 1:geno.num) svd.mat[i,] <- svd.mat[i,] - env.mean
   }
-  
+
   if (method == "GGE"){
     svd.mat <- int.mean
     for (i in 1:geno.num) svd.mat[i,] <- svd.mat[i,] - env.mean
   }
-  
+
   # SVD
-  
+
   PC <- min(env.num, geno.num)-1
   dec <- svd(svd.mat, nu = PC, nv = PC)
   D <- diag(dec$d[1:PC])
@@ -185,15 +185,15 @@ AMMIwithMeans <- function(int.mean, trait = NULL, rep.num = NULL, rdf = NULL,
   dimnames(PC.env) <- list(colnames(int.mean), c("PC1", "PC2"))
   PC.num <- paste("PC", c(1:PC), sep = "")
   PC.sv <- dec$d[1:PC]^2
-  
+
   # Contribution of PCs
-  
+
   PC.cont <- PC.sv/sum(PC.sv)*100
   PC.acum <- cumsum(PC.cont)
   tablaPC <- data.frame(PC = PC.num, SV = PC.sv, Cont = PC.cont, CumCont = PC.acum)
-  
+
   # Significance of PCs, only for AMMI and if rep.num, rms and rdf are known
-  
+
   if (method == "AMMI"){
     if (is.null(rep.num) == 0){
       int.SS <- (t(as.vector(svd.mat))%*%as.vector(svd.mat))*rep.num
@@ -209,14 +209,14 @@ AMMIwithMeans <- function(int.mean, trait = NULL, rep.num = NULL, rdf = NULL,
       colnames(tablaPC)[5:9] <- c("df", "SumSq", "MeanSq", "Fvalue", "Pr(>F)")
     }
   }
-  
+
   #  Biplot 1
-  
+
   if (biplot == 1){
-    
+
     if (is.null(title) == 1)
       title = paste(method, " biplot1 for ", trait, sep = "")
-    
+
     if (biplot1 == "effects"){
       maxx <- max(abs(c(env.mean - overall.mean, geno.mean - overall.mean)))*1.05
       limx <- c(-maxx, maxx)
@@ -235,9 +235,9 @@ AMMIwithMeans <- function(int.mean, trait = NULL, rep.num = NULL, rdf = NULL,
       xcore = env.mean
       xline = overall.mean
     }
-    
+
     limy <- c(-max(abs(c(E[,1], G[,1]))), max(abs(c(E[,1], G[,1]))))
-    
+
     plot(1, type = "n", xlim = limx, ylim = limy, main = title, xlab = xlab,
          ylab = paste("PC1 (",format(PC.cont[1],digits = 3),"%)"), ...)
     points(xcorg, G[,1], col = color[2], pch = 17, ...)
@@ -246,18 +246,18 @@ AMMIwithMeans <- function(int.mean, trait = NULL, rep.num = NULL, rdf = NULL,
     text(xcore, E[,1], labels = colnames(int.mean), col = color[1], pos = 1, offset = .3)
     abline(h = 0, v = xline, col = color[3], lty = 2)
   }
-  
+
   # Biplot 2
-  
+
   if (biplot == 2){
-    
+
     if (is.null(title) == 1)
       title = paste(method, " biplot2 for ", trait, sep = "")
-    
+
     limx <- range(c(E[,1], G[,1]))
     limx <- limx + c(-max(abs(limx)), max(abs(limx)))*.05
     limy <- range(c(E[,2], G[,2]))
-    
+
     plot(1, type = "n", xlim = limx, ylim = limy, main = title,
          xlab = paste("PC1 (", format(PC.cont[1], digits = 3), "%)"),
          ylab = paste("PC2 (", format(PC.cont[2], digits = 3), "%)"),
@@ -269,9 +269,9 @@ AMMIwithMeans <- function(int.mean, trait = NULL, rep.num = NULL, rdf = NULL,
     abline(h = 0, v = 0, col = color[3], lty = 2)
     for (i in 1:env.num) lines(c(0,E[i,1]), c(0,E[i,2]), col = color[1], lty = 3)
   }
-  
+
   # Output
-  
+
   list(PC_values_genotypes = PC.geno, PC_values_environments = PC.env,
        Contribution_PCs = tablaPC)
 }
